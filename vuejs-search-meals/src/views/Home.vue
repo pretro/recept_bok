8e918a069b515178272c4bbc351413056cee5490
jort2 <>
2023-08-18 23:00:38 +0200

    Created MealList.vue in views folder

--- vuejs-search-meals/src/views/Home.vue
+++ vuejs-search-meals/src/views/Home.vue
@@ -1,5 +1,20 @@
 <template>
-  <input type="text">
+  <div class="flex flex-col p-8 items-center justify-center">
+    <input
+      type="text"
+      class="rounded border-2 border-gray-200 w-full"
+      placeholder="Sök efter måltid"
+    />
+    <div class="flex justify-center gap-2 mt-2">
+      <router-link
+        :to="{name: 'byLetter', params: {letter}}"
+        v-for="letter of letters"
+        :key="letter"
+      >
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -7,4 +22,5 @@ import { computed } from "vue";
 import store from "../store";
 
 const meals = computed(() => store.state.meals);
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 </script>

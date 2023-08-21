e1d4fb3d62230e329b84fa2b30cd99b78a1af103
jort2 <>
2023-08-21 12:38:39 +0200

    Creating MealDetail view part 4

--- vuejs-search-meals/src/components/YouTubeButton.vue
+++ vuejs-search-meals/src/components/YouTubeButton.vue
@@ -1,10 +1,10 @@
 <template>
-    <a :href="meal.strYoutube" target="_blank"
-        class="px-3 py-2 rounded border-2 border-red-600 text-white bg-red-500 hover:bg-red-600 transition-colors">
-        YouTube
+    <a :href="href" target="_blank"
+        class="px-3 py-2 rounded border-2 text-white border-orange-600 bg-orange-500 hover:bg-orange-600 transition-colors">
+        <slot>YouTube</slot>
     </a>
 </template>
-
+  
 <script setup>
 const props = defineProps({
     href: String
